c678c2a5249a524c66e73b9e9ae556b2d97c53c5
Erika Åsberg <erikaasberg@Erikas-MacBook-Pro.local>
2020-06-29 13:43:00 +0200

    Updates

---
+++ precache-manifest.ed80a2a72468d4260b729f78a4a333a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7bdd44e78cb38bee1b187ed4185c2f",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "7dcb04403c4f5dbcd3a8",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3a3d66db4f7e1201288",
+    "url": "/infoskjerm/static/js/2.3486e9a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.3486e9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcb04403c4f5dbcd3a8",
+    "url": "/infoskjerm/static/js/main.6a014117.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
