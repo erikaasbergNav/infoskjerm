0d5e8b5eb8c02fad7c0a2bb33108aae29922d193
Erika Åsberg <erikaasberg@Erikas-MacBook-Pro.local>
2020-06-29 13:34:16 +0200

    satt opp infoskjerm

---
+++ src/Reddit.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+
+const APIUrl = "https://www.reddit.com/r/dadjokes/top.json?t=day&limit=1";
+
+const Reddit = () => {
+    const [joke, setJoke] = useState({});
+
+    const getJoke = () => {
+        fetch(APIUrl)
+            .then((response) => response.json())
+            .then((json) => setJoke(json.data.children[0].data))
+            .catch((err) => console.error(err))
+    }
+
+    
+    // init, hvis komponenten rendres gjør dette kallet (KUN NÅR DEN RENDRES), variabelen inni [] endres ved render
+    useEffect(() => {
+        getJoke();
+    }, []);
+
+    return joke ? (  
+        <div className = "reddit" >
+            <p>{joke.title}</p>
+            <p>{joke.selftext}</p>
+        </div>
+    ) : (
+            <></>
+    )
+}
+
+export default Reddit;
